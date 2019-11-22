74734fc9afe665ddd936ba5ec1c8d303033c17de
aarjav89 <53356396+aarjav89@users.noreply.github.com>
2019-11-21 19:24:29 -0500

    Add project files.

---
+++ PizzaPizzaOrderForm/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PizzaPizzaOrderForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int crust =0, toppings = 0, addon = 0;
+        public double size = 0.0, total = 0.0;
+        public int tax = 13;
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            size = 8.99;
+        }
+    }
+}
